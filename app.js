28065ccb6c710399ac1c21fb1ffd3e94ac03f628
meiCiEn <99802093+meiCiEn@users.noreply.github.com>
2025-06-10 16:38:43 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const library = {
+    // array to store book objects
+    _books: [
+        {
+            title: "Dune",
+            author: "Frank Herbert",
+            pages: 412,
+            isRead: false
+        },
+        {
+            title: "Pride and Prejudice",
+            author: "Jane Austen",
+            pages: 432,
+            isRead: true
+        },
+        {
+            title: "1984",
+            author: "George Orwell",
+            pages: 328,
+            isRead: false
+        }
+    ]
+};
